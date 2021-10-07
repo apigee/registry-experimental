ddf5b47f61e7111914bf2539dba40746c32acb0e
Tim Burks <timburks@google.com>
2021-10-06 20:35:25 -0700

    Create "registry-experimental" tool, initially containing bleve-based search experiments.

---
+++ cmd/registry-experimental/cmd/root.go
@@ -0,0 +1,52 @@
+// Copyright 2020 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+
+	"fmt"
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/text"
+	"github.com/apigee/registry-experimental/cmd/registry-experimental/cmd/compute"
+	"github.com/apigee/registry-experimental/cmd/registry-experimental/cmd/search"
+	"github.com/google/uuid"
+	"github.com/spf13/cobra"
+)
+
+func Command(ctx context.Context) *cobra.Command {
+	var logID string
+	var cmd = &cobra.Command{
+		Use:   "registry-experimental",
+		Short: "Experimental utilities for working with the API Registry",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			// Initialize global default logger with unique process identifier.
+			if len(logID) == 0 {
+				logID = fmt.Sprintf("[ %.8s ] ", uuid.New())
+			}
+			logger := &log.Logger{
+				Level:   log.DebugLevel,
+				Handler: text.Default,
+			}
+			log.Log = logger.WithField("uid", logID)
+		},
+	}
+
+	cmd.AddCommand(compute.Command(ctx))
+	cmd.AddCommand(search.Command(ctx))
+
+	cmd.PersistentFlags().StringVar(&logID, "log-id", "", "Assign an ID which gets attached to the log produced")
+	return cmd
+}

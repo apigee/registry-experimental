4706807807d40ea9fe9fd5ad18ce52c570041057
Tim Burks <timburks@google.com>
2021-11-24 09:53:01 -0800

    "generate client" calls Go Discovery client generator.

--- cmd/registry-experimental/cmd/generate/client.go
+++ cmd/registry-experimental/cmd/generate/client.go
@@ -17,6 +17,8 @@ package generate
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
 
 	"github.com/apex/log"
 	"github.com/apigee/registry/cmd/registry/core"
@@ -82,5 +84,75 @@ func (task *generateClientTask) String() string {
 
 func (task *generateClientTask) Run(ctx context.Context) error {
 	log.FromContext(ctx).Info(task.String())
+	request := &rpc.GetApiSpecRequest{
+		Name: task.specName,
+	}
+	spec, err := task.client.GetApiSpec(ctx, request)
+	if err != nil {
+		return err
+	}
+	var client []byte
+	relation := "client"
+	if core.IsDiscovery(spec.GetMimeType()) {
+		log.FromContext(ctx).Debugf("Computing %s/specs/%s", spec.Name, relation)
+		data, err := core.GetBytesForSpec(ctx, task.client, spec)
+		if err != nil {
+			return err
+		}
+		client, err = clientFromDiscovery(spec.Name, data, task.language)
+		if err != nil {
+			log.FromContext(ctx).WithError(err).Warnf("error generating client for: %s", spec.Name)
+			return nil
+		}
+	} else {
+		log.FromContext(ctx).Infof("we don't know how to generate OpenAPI for %s", spec.Name)
+		return nil
+	}
+	subject := spec.GetName()
+	artifact := &rpc.Artifact{
+		Name:     subject + "/artifacts/" + relation,
+		MimeType: "application/octet-stream",
+		Contents: client,
+	}
+	err = core.SetArtifact(ctx, task.client, artifact)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+// clientFromDiscovery runs a discovery code generator and returns the results.
+func clientFromDiscovery(name string, b []byte, language string) ([]byte, error) {
+	// create a tmp directory
+	root, err := ioutil.TempDir("", "registry-generator-")
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("WORKING IN %s\n", root)
+	// whenever we finish, delete the tmp directory
+	//defer os.RemoveAll(root)
+
+	// write the spec into the temp directory
+	ioutil.WriteFile(root+"/discovery.json", b, 0444)
+
+	// run the code generator
+	cmd := exec.Command("google-api-go-generator",
+		"-api_json_file",
+		"discovery.json",
+		"-gendir",
+		"client")
+	cmd.Dir = root
+	err = cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	// zip the result
+	cmd = exec.Command("zip", "-r", "client.zip", "client")
+	cmd.Dir = root
+	err = cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadFile(root + "/client.zip")
 }

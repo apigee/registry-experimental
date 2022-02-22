43c2cdd490b4bc6bcf4e46d97df19554983ced8f
Gitesh Koli <giteshk@users.noreply.github.com>
2022-02-22 17:40:17 -0500

    Change to autodetect renderer from mimetype of the spec (#24)
    * Renderer chooses the correct library based on mimetype of the spec
    
    * Renderer chooses the correct library based on mimetype of the spec

--- containers/registry-spec-renderer/index.js
+++ containers/registry-spec-renderer/index.js
@@ -1,85 +1,136 @@
 const express = require('express')
 const fs = require("fs");
 const handlebars = require("handlebars");
-const { RegistryClient } = require("@giteshk-org/apigeeregistry");
+const {RegistryClient} = require("@giteshk-org/apigeeregistry");
 const grpc = require("@grpc/grpc-js");
 
 var client_options = {};
 if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-    client_options.sslCreds = grpc.credentials.createInsecure();
+  client_options.sslCreds = grpc.credentials.createInsecure();
 }
 
 if (process.env.APG_REGISTRY_ADDRESS) {
-    items = process.env.APG_REGISTRY_ADDRESS.split(":");
-    client_options.apiEndpoint = items[0];
-    client_options.port = items.length >= 1 ? items[1] : 443;
+  items = process.env.APG_REGISTRY_ADDRESS.split(":");
+  client_options.apiEndpoint = items[0];
+  client_options.port = items.length >= 1 ? items[1] : 443;
 }
 
 const client = new RegistryClient(client_options);
 
-const swagger_ui_template = fs.readFileSync("renderers/swagger-ui.html.template");
+// Read the templates files for various api formats.
+const swagger_ui_template = fs.readFileSync(
+    "renderers/swagger-ui.html.template");
 const graphiql_template = fs.readFileSync("renderers/graphiql.html.template");
 const async_template = fs.readFileSync("renderers/async-ui.html.template");
 
 const app = express();
 
-app.use('/renderer/openapi', express.static(require('swagger-ui-dist').absolutePath()))
+//Serve static assets for openapi renderer
+app.use('/renderer/openapi',
+    express.static(require('swagger-ui-dist').absolutePath()))
 
-app.use('/renderer/async', express.static('node_modules/@webcomponents/webcomponentsjs'))
-app.use('/renderer/async', express.static('node_modules/@asyncapi/web-component'))
- app.use('/renderer/async', express.static('node_modules/@asyncapi/react-component'))
+//Serve static assets for asyncapi renderer
+app.use('/renderer/async',
+    express.static('node_modules/@webcomponents/webcomponentsjs'))
+app.use('/renderer/async',
+    express.static('node_modules/@asyncapi/web-component'))
+app.use('/renderer/async',
+    express.static('node_modules/@asyncapi/react-component'))
 
+//Serve static assets for graphql renderer
 app.use('/renderer/graphql', express.static('node_modules/react'))
 app.use('/renderer/graphql', express.static('node_modules/react-dom'))
 app.use('/renderer/graphql', express.static('node_modules/graphiql'))
 
-app.get('/renderer/:apiformat/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId', (req, res) => {
-    res.setHeader("content-type", "text/html; charset=UTF-8");
-    spec_url = "/spec/projects/" + req.params.projectId + "/locations/" + req.params.locationId + "/apis/" + req.params.apiId + "/versions/" + req.params.versionId + "/specs/" + req.params.specId;
-    renderer_template = "";
-    switch (req.params.apiformat) {
-        case "openapi":
-            renderer_template = swagger_ui_template.toString();
-            break;
-        case "async":
-            renderer_template = async_template.toString();
-            break;
-        case "graphql":
-            renderer_template = graphiql_template.toString();
-            break;
-    }
-    hbstemplate = handlebars.compile(renderer_template);
-    res.send(hbstemplate({ specUrl: spec_url }));
-    res.end();
-});
+/**
+ * Render an API Spec from registry.
+ * Chooses the renderer to use based on mimeType of the Spec
+ */
+app.get(
+    '/render/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId',
+    (req, res) => {
+      spec_name = "projects/" + req.params.projectId + "/locations/"
+          + req.params.locationId + "/apis/" + req.params.apiId + "/versions/"
+          + req.params.versionId + "/specs/" + req.params.specId;
 
-app.all("/spec/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId", (req, res) => {
-    if (req.method !== 'POST' && req.method !== 'GET') {
+      client.getApiSpec({
+        name: spec_name
+      }, (err, response) => {
+        if (err) {
+          res.sendStatus(500);
+          res.end();
+        } else {
+          let apiFormat = '';
+          if (response.mimeType.includes("openapi")) {
+            apiFormat = 'openapi';
+          } else if (response.mimeType.includes("asyncapi")) {
+            apiFormat = 'asyncapi';
+          } else if (response.mimeType.includes("discovery")) {
+            apiFormat = 'discovery';
+          } else if (response.mimeType.includes("proto")) {
+            apiFormat = 'proto';
+          } else if (response.mimeType.includes("graphql")) {
+            apiFormat = 'graphql';
+          }
+          var renderer_template = "";
+          switch (apiFormat) {
+            case "openapi":
+              renderer_template = swagger_ui_template.toString();
+              break;
+            case "async":
+              renderer_template = async_template.toString();
+              break;
+            case "graphql":
+              renderer_template = graphiql_template.toString();
+              break;
+          }
+          specUrl = "/spec/" + spec_name;
+          if (renderer_template != "") {
+            res.setHeader("content-type", "text/html; charset=UTF-8");
+            hbstemplate = handlebars.compile(renderer_template);
+            res.send(hbstemplate({specUrl: specUrl}));
+          } else {
+            res.redirect(specUrl);
+          }
+          res.end();
+        }
+      })
+    });
+
+/**
+ * Return the contents of an API Spec
+ */
+app.all(
+    "/spec/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId",
+    (req, res) => {
+      if (req.method !== 'POST' && req.method !== 'GET') {
         res.sendStatus(404);
         res.end();
         return;
-    }
+      }
 
-    let spec_url = "projects/" + req.params.projectId + "/locations/" + req.params.locationId + "/apis/" + req.params.apiId + "/versions/" + req.params.versionId + "/specs/" + req.params.specId;
+      let spec_url = "projects/" + req.params.projectId + "/locations/"
+          + req.params.locationId + "/apis/" + req.params.apiId + "/versions/"
+          + req.params.versionId + "/specs/" + req.params.specId;
 
-    client.getApiSpecContents(
-        {
+      client.getApiSpecContents(
+          {
             name: spec_url
-        }, (err, response) => {
+          }, (err, response) => {
             if (err) {
-                console.error(err);
-                res.sendStatus(500);
-                res.end();
+              console.error(err);
+              res.sendStatus(500);
+              res.end();
             } else {
-                res.setHeader("content-type", response.contentType);
-                res.send(response.data);
-                res.end();
+              res.setHeader("content-type", response.contentType);
+              res.send(response.data);
+              res.end();
             }
-        })
-});
+          })
+    });
 
-app.get('/healthz', (req, res) =>{
-    res.sendStatus(200);
-    res.end();
+app.get('/healthz', (req, res) => {
+  res.sendStatus(200);
+  res.end();
 });
 app.listen(process.env.PORT || 3000);

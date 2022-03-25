cd16d2653341e9841f9c0e38113350c1b857eac4
Gitesh Koli <giteshk@users.noreply.github.com>
2022-03-24 23:11:29 -0400

    Detect api style based on mimetype or apihub-style label (#28)
    * Added the ability to detect renderer based on the apihub-style label
    Added documentation to allow for running the spec renderer on GCE
    
    * published the apigee registry nodejs gapic client to npm public repository.
    So we don't have to use github container registry
    
    * Added the ability to add mock-endpoint from the artifacts for the spec
    Added ability to create an https endpoint so Identity Aware proxy can be enabled on the service

--- containers/registry-spec-renderer/README.md
+++ containers/registry-spec-renderer/README.md
@@ -1,7 +1,32 @@
 # Registry Spec renderer
 This container allows for rendering of specs from the API Registry.
 
-### To run this against a self hosted version of Apigee Registry you will need to 
+### To run this service on a GCE instance run the following command:
+```
+export REGISTRY_PROJECT_IDENTIFIER=$(gcloud config list --format 'value(core.project)')
+gcloud iam service-accounts create registry-reader \
+    --description="Registry Reader" \
+    --display-name="Registry Reader"
+
+gcloud projects add-iam-policy-binding $REGISTRY_PROJECT_IDENTIFIER \
+    --member="serviceAccount:registry-reader@$REGISTRY_PROJECT_IDENTIFIER.iam.gserviceaccount.com" \
+    --role="roles/apigeeregistry.viewer"
+
+gcloud compute firewall-rules create registry-renderer-service-fw \
+    --action allow \
+    --target-tags registry-spec-renderer \
+    --source-ranges 0.0.0.0/0 \
+    --rules tcp:80
+
+
+gcloud compute instances create-with-container registry-renderer-instance1 \
+	--machine-type=e2-micro  --tags=registry-spec-renderer,http-server \
+	--scopes=https://www.googleapis.com/auth/cloud-platform \
+	--restart-on-failure --service-account=registry-reader@$REGISTRY_PROJECT_IDENTIFIER.iam.gserviceaccount.com\
+    --container-image ghcr.io/apigee/registry-spec-renderer:latest
+```
+
+### To run this against the opensource version of Apigee Registry on GKE you will need to:  
 
 1. Create a namespace for registry-spec-renderer
     ```
@@ -17,7 +42,9 @@ This container allows for rendering of specs from the API Registry.
     kubectl apply -f kubernetes/deployment-self-hosted.yaml -n registry-spec-renderer
    ```
 
-### Running this service against hosted API Registry service 
+### Running this service against hosted API Registry service on GKE: 
+
+*Instead of using key you can configure workload identity*
 1. you will need to create a service account with the 'roles/apigeeregistry.viewer' role
 
 2. You can download the service account key and rename the file to service-account.json
@@ -31,7 +58,16 @@ This container allows for rendering of specs from the API Registry.
    kubectl create secret generic registry-spec-renderer-sa-key \
    --from-file service-account.json -n registry-spec-renderer
    ```
-5. Apply the deployment 
+5. Assign a static IP for your Service
+   ```shell
+       gcloud compute addresses create registry-spec-renderer-static-ip \
+       --global \
+       --ip-version IPV4
+   ```
+6. Update the cert domain in the deployment.yaml with either sslip.io or 
+   a custom domain
+
+7. Apply the deployment 
    ```
     kubectl apply -f kubernetes/deployment.yaml -n registry-spec-renderer
    ```
--- containers/registry-spec-renderer/index.js
+++ containers/registry-spec-renderer/index.js
@@ -1,13 +1,34 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const express = require('express')
 const fs = require("fs");
 const handlebars = require("handlebars");
 const {RegistryClient} = require("@giteshk-org/apigeeregistry");
 const grpc = require("@grpc/grpc-js");
+const jsYaml = require('js-yaml');
+var cors = require('cors')
 
 var client_options = {};
-if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+if (process.env.APG_REGISTRY_INSECURE
+    && process.env.APG_REGISTRY_INSECURE == "1") {
   client_options.sslCreds = grpc.credentials.createInsecure();
 }
+
+const MOCK_ENDPOINT_ARTIFACT_NAME = process.env.MOCK_ENDPOINT_ARTIFACT_NAME
+    || "mock-endpoint";
 
 if (process.env.APG_REGISTRY_ADDRESS) {
   items = process.env.APG_REGISTRY_ADDRESS.split(":");
@@ -24,6 +45,7 @@ const graphiql_template = fs.readFileSync("renderers/graphiql.html.template");
 const async_template = fs.readFileSync("renderers/async-ui.html.template");
 
 const app = express();
+app.use(cors())
 
 //Serve static assets for openapi renderer
 app.use('/renderer/openapi',
@@ -42,6 +64,49 @@ app.use('/renderer/graphql', express.static('node_modules/react'))
 app.use('/renderer/graphql', express.static('node_modules/react-dom'))
 app.use('/renderer/graphql', express.static('node_modules/graphiql'))
 
+function renderTemplate(res, apiFormat, spec_name) {
+  var renderer_template = "";
+  switch (apiFormat) {
+    case "openapi":
+      renderer_template = swagger_ui_template.toString();
+      break;
+    case "asyncapi":
+      renderer_template = async_template.toString();
+      break;
+    case "graphql":
+      renderer_template = graphiql_template.toString();
+      break;
+    default:
+      renderer_template = "spec_file"
+      break;
+  }
+  specUrl = "/spec/" + apiFormat + "/" + spec_name;
+  if (renderer_template != "spec_file") {
+    res.setHeader("content-type", "text/html; charset=UTF-8");
+    hbstemplate = handlebars.compile(renderer_template);
+    res.send(hbstemplate({specUrl: specUrl}));
+  } else {
+    res.redirect(specUrl);
+  }
+  res.end();
+}
+
+function getAPIFormat(text) {
+  let apiFormat = '';
+  if (text.includes("openapi")) {
+    apiFormat = 'openapi';
+  } else if (text.includes("asyncapi")) {
+    apiFormat = 'asyncapi';
+  } else if (text.includes("discovery")) {
+    apiFormat = 'discovery';
+  } else if (text.includes("proto")) {
+    apiFormat = 'proto';
+  } else if (text.includes("graphql")) {
+    apiFormat = 'graphql';
+  }
+  return apiFormat;
+}
+
 /**
  * Render an API Spec from registry.
  * Chooses the renderer to use based on mimeType of the Spec
@@ -49,50 +114,40 @@ app.use('/renderer/graphql', express.static('node_modules/graphiql'))
 app.get(
     '/render/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId',
     (req, res) => {
-      spec_name = "projects/" + req.params.projectId + "/locations/"
-          + req.params.locationId + "/apis/" + req.params.apiId + "/versions/"
+      api_name = "projects/" + req.params.projectId + "/locations/"
+          + req.params.locationId + "/apis/" + req.params.apiId;
+      spec_name = api_name + "/versions/"
           + req.params.versionId + "/specs/" + req.params.specId;
 
       client.getApiSpec({
         name: spec_name
       }, (err, response) => {
         if (err) {
+          console.error(err);
           res.sendStatus(500);
           res.end();
         } else {
-          let apiFormat = '';
-          if (response.mimeType.includes("openapi")) {
-            apiFormat = 'openapi';
-          } else if (response.mimeType.includes("asyncapi")) {
-            apiFormat = 'asyncapi';
-          } else if (response.mimeType.includes("discovery")) {
-            apiFormat = 'discovery';
-          } else if (response.mimeType.includes("proto")) {
-            apiFormat = 'proto';
-          } else if (response.mimeType.includes("graphql")) {
-            apiFormat = 'graphql';
-          }
-          var renderer_template = "";
-          switch (apiFormat) {
-            case "openapi":
-              renderer_template = swagger_ui_template.toString();
-              break;
-            case "async":
-              renderer_template = async_template.toString();
-              break;
-            case "graphql":
-              renderer_template = graphiql_template.toString();
-              break;
-          }
-          specUrl = "/spec/" + spec_name;
-          if (renderer_template != "") {
-            res.setHeader("content-type", "text/html; charset=UTF-8");
-            hbstemplate = handlebars.compile(renderer_template);
-            res.send(hbstemplate({specUrl: specUrl}));
+          apiFormat = getAPIFormat(response.mimeType);
+          if (apiFormat == '') {
+            client.getApi({
+              name: api_name
+            }, (err, response2) => {
+              if (err) {
+                console.error(err);
+                res.sendStatus(500);
+                res.end();
+              } else {
+                apiFormat = '';
+                if (response2.labels['apihub-style']) {
+                  apiFormat = getAPIFormat(
+                      response2.labels['apihub-style']);
+                }
+                renderTemplate(res, apiFormat, spec_name);
+              }
+            });
           } else {
-            res.redirect(specUrl);
+            renderTemplate(res, apiFormat, spec_name);
           }
-          res.end();
         }
       })
     });
@@ -101,7 +156,7 @@ app.get(
  * Return the contents of an API Spec
  */
 app.all(
-    "/spec/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId",
+    "/spec/:specType/projects/:projectId/locations/:locationId/apis/:apiId/versions/:versionId/specs/:specId",
     (req, res) => {
       if (req.method !== 'POST' && req.method !== 'GET') {
         res.sendStatus(404);
@@ -122,15 +177,67 @@ app.all(
               res.sendStatus(500);
               res.end();
             } else {
-              res.setHeader("content-type", response.contentType);
-              res.send(response.data);
-              res.end();
+              specObj = {};
+              if (req.params.specType == 'openapi') {
+                try {
+                  specObj = JSON.parse(response.data);
+                } catch {
+                  specObj = jsYaml.load(response.data);
+                }
+                return addMockAddressForOpenAPI(specObj, spec_url, res);
+              } else {
+                res.setHeader("content-type", response.contentType);
+                res.send(response.data);
+                res.end();
+              }
             }
           })
     });
+
+/**
+ * Check if the mock-server artifact exists on the spec.
+ * If it does add that to the list of servers
+ *
+ * @param openAPISpecObj
+ * @param spec_url
+ * @param res
+ */
+function addMockAddressForOpenAPI(openAPISpecObj, spec_url, res) {
+  client.getArtifactContents({
+    name: spec_url + "/artifacts/" + MOCK_ENDPOINT_ARTIFACT_NAME
+  }).then(arr => {
+    arr.forEach((body) => {
+      if (body.data) {
+        mockendpoint = body.data.toString('utf8')
+        if (mockendpoint.length > 0) {
+          if (openAPISpecObj.swagger && openAPISpecObj.swagger.startsWith("2") && !openAPISpecObj.host) {
+            openAPISpecObj.host = mockendpoint;
+          } else if (openAPISpecObj.openapi && openAPISpecObj.openapi.startsWith("3")) {
+            openAPISpecObj = openAPISpecObj || {
+              servers: []
+            }
+            openAPISpecObj.servers.push(
+                {url: mockendpoint, description: "Mock endpoint"});
+          }
+        }
+      }
+    });
+    res.json(openAPISpecObj);
+    res.end();
+  }).catch(() => {
+    res.json(openAPISpecObj);
+    res.end();
+  })
+}
 
 app.get('/healthz', (req, res) => {
   res.sendStatus(200);
   res.end();
 });
-app.listen(process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 80, function (err) {
+  if (err) {
+    console.log("Error in server setup")
+  }
+  console.log("Server listening on Port", process.env.PORT || 80);
+});

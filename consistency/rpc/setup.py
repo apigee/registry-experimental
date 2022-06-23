7a7b9eef1adf0906561ef1e13864dfb330ee13b4
shrutiparabgoogle <shrutiparab@google.com>
2022-06-23 14:19:24 -0700

    Add generated code for vocabulary protos and an example

--- consistency/rpc/setup.py
+++ consistency/rpc/setup.py
@@ -8,7 +8,6 @@ package_root = os.path.abspath(os.path.dirname(__file__))
 setuptools.setup(
     name='google-cloud-apigeeregistry',
     packages=setuptools.PEP420PackageFinder.find(),
-    namespace_packages=('google', 'google.cloud'),
     install_requires=(
         'google-api-core[grpc] >= 1.31.0, < 3.0.0dev',
         'googleapis-common-protos >= 1.55.0, <2.0.0dev',
--- tools/GENERATE-PYTHON.sh
+++ tools/GENERATE-PYTHON.sh
@@ -8,6 +8,13 @@ then
     git clone https://github.com/apigee/registry $REGISTRY_PROTOS_PATH
 fi
 
+# clone vocabulary protos
+GNOSTIC_PROTOS_PATH='consistency/gnostic-protos'
+if [ ! -d $GNOSTIC_PROTOS_PATH ]
+then
+    git clone https://github.com/google/gnostic $GNOSTIC_PROTOS_PATH
+fi
+
 # clone common protos
 COMMON_PROTOS_PATH='third_party/api-common-protos'
 if [ ! -d $COMMON_PROTOS_PATH ]
@@ -22,6 +29,7 @@ ALL_PROTOS=(
 	$REGISTRY_PROTOS_PATH/google/cloud/apigeeregistry/v1/controller/*.proto
 	$REGISTRY_PROTOS_PATH/google/cloud/apigeeregistry/v1/apihub/*.proto
 	$REGISTRY_PROTOS_PATH/google/cloud/apigeeregistry/v1/scoring/*.proto
+	$GNOSTIC_PROTOS_PATH/metrics/*.proto
 )
 SERVICE_PROTOS=(
 	$REGISTRY_PROTOS_PATH/google/cloud/apigeeregistry/v1/registry_models.proto
@@ -35,6 +43,6 @@ PYTHON_TYPES_PATH='consistency/rpc'
 # # Generating grpc clients 
 pip install grpcio-tools
 echo "Generating gRPC client for ${ALL_PROTOS[*]}"
-python -m grpc_tools.protoc --proto_path=$REGISTRY_PROTOS_PATH --proto_path=$COMMON_PROTOS_PATH --python_out=$PYTHON_TYPES_PATH --grpc_python_out=$PYTHON_TYPES_PATH ${ALL_PROTOS[*]}
+python -m grpc_tools.protoc --proto_path=$REGISTRY_PROTOS_PATH --proto_path=$COMMON_PROTOS_PATH --proto_path=$GNOSTIC_PROTOS_PATH --python_out=$PYTHON_TYPES_PATH --grpc_python_out=$PYTHON_TYPES_PATH ${ALL_PROTOS[*]}
 
 pip install consistency/rpc

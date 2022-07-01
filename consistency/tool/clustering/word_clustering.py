77c1e3a682a12753f75260384e9f6b15abae171b
abenezer-gelaw <gelaw@google.com>
2022-07-01 15:15:25 -0700

    basic clustering complete

--- consistency/tool/clustering/word_clustering.py
+++ consistency/tool/clustering/word_clustering.py
@@ -1,88 +1,64 @@
+import word_extracting
 import grpc
+import numpy as np
+import pandas as pd
+from sklearn.cluster import dbscan
+from strsimpy.sorensen_dice import SorensenDice
+from sklearn.cluster import dbscan
 from google.cloud.apigeeregistry.v1 import registry_service_pb2
 from metrics import vocabulary_pb2
- 
-class ExtractWords:
-   def __init__(self, stub, linearize=True):
-       self.stub = stub
-       self.linearize = linearize
- 
-   def extract_words(self):
- 
-       stub = self.stub
-       linearize = self.linearize
- 
-       try:
-           response = stub.ListArtifacts(
-               registry_service_pb2.ListArtifactsRequest(
-                   parent="projects/-/locations/global/apis/-/versions/-/specs/-",
-                   filter="name.contains(\"vocabulary\")"
-               ))
-       except grpc.RpcError as rpc_error:
-           print(f"Received RPC error: code={rpc_error.code()} message={rpc_error.details()}")
-           
-       words = [] if linearize else {}
+from google.cloud.apigeeregistry.v1 import registry_service_pb2_grpc
+from google.cloud.apigeeregistry.v1 import registry_service_pb2
+from metrics import vocabulary_pb2
+from collections import Counter
 
-       for artifact in response.artifacts:
-           contents = stub.GetArtifactContents(
-               registry_service_pb2.GetArtifactContentsRequest(
-                   name=artifact.name,
-               )
-           )
- 
-           vocab = vocabulary_pb2.Vocabulary()
-           vocab.ParseFromString(contents.data)
 
-           # Linearize all words into a list 
-           if linearize:
-               for entry in vocab.schemas:
-                   for _ in range(entry.count):
-                       words.append(entry.word)
- 
-               for entry in vocab.properties:
-                   for _ in range(entry.count):
-                       words.append(entry.word)
- 
-               for entry in vocab.operations:
-                   for _ in range(entry.count):
-                       words.append(entry.word)
- 
-               for entry in vocab.parameters:
-                   for _ in range(entry.count):
-                       words.append(entry.word)
-               return words
 
-           # Put words in a map frequencies.   
-           else:
-               for entry in vocab.schemas:
-                   word = entry.word
-                   count= entry.count
-                   if word not in words:
-                       words[word] = 0
-                   words[word] += count
- 
-               for entry in vocab.properties:
-                   word = entry.word
-                   count= entry.count
-                   if word not in words:
-                       words[word] = 0
-                   words[word] += count
- 
-               for entry in vocab.operations:
-                   word = entry.word
-                   count= entry.count
-                   if word not in words:
-                       words[word] = 0
-                   words[word] += count
- 
-               for entry in vocab.parameters:
-                   word = entry.word
-                   count= entry.count
-                   if word not in words:
-                       words[word] = 0
-                   words[word] += count
-               return words
- 
- 
- 
+class ClusterWords:
+
+    def __init__(self, words, linearized = True):
+       self.words = words
+       self.linearized = linearized
+       self.eps = .3 
+       self.dbscan_min_samples = 2
+       self.dice = SorensenDice(2)
+       
+    def linearize_words(self):
+        linearized = self.linearized
+        words = self.words
+        data = []
+        if not linearized:
+            for key in words:
+                for _ in range(words[key]):
+                    data.append(key)
+        else:
+            data = words
+        return data
+
+    def extract_indices_dice(self, x, y, data):
+        i, j = int(x[0]), int(y[0])     # extract indices
+        return self.dice.distance(data[i], data[j])
+
+    def extract_words (self):
+        data = self.linearize_words()
+        data = np.arange(len(data)).reshape(-1, 1)
+
+
+        db = dbscan(data, metric=self.extract_indices_dice, eps=self.eps, min_samples = self.dbscan_min_samples, algorithm='brute')
+        labels = db[1]
+        counts = Counter(labels)
+        clusters = dict()
+
+        for i, n in enumerate(labels):
+            if counts[n]>1:
+                clusters.setdefault(n,[]).append(i)
+
+        for key in clusters:
+            similar_words = list(clusters[key])
+            cluster_counts = Counter(similar_words)
+            new_key = max(similar_words, key=cluster_counts.get)
+            clusters[new_key] = clusters.pop(key)
+
+        return clusters
+
 

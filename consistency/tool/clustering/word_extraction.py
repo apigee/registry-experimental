9bdd6736dc6db86772af849f3fee2509b184b9ee
abenezer-gelaw <gelaw@google.com>
2022-07-12 16:35:10 -0700

    param complete

--- consistency/tool/clustering/word_extraction.py
+++ consistency/tool/clustering/word_extraction.py
@@ -1,13 +1,10 @@
-from urllib import response
 import grpc
 from google.cloud.apigeeregistry.v1 import registry_service_pb2
 from metrics import vocabulary_pb2
 
 class ExtractWords:
-    def __init__(self, stub, linearize=True):
+    def __init__(self, stub):
       self.stub = stub
-      self.linearize = linearize
-
 
     def extract_vocabs(self):
       stub = self.stub
@@ -21,7 +18,6 @@ class ExtractWords:
           print(f"Received RPC error: code={rpc_error.code()} message={rpc_error.details()}") 
 
       vocabs = []
-    
      
       for artifact in response.artifacts:
           contents = stub.GetArtifactContents(
@@ -37,54 +33,20 @@ class ExtractWords:
 
     def get_vocabs(self):
         vocabs = self.extract_vocabs(self)
-
-        words = [] if self.linearize else {}
-          
-        if self.linearize:
-            for vocab in vocabs:   
-                for entry in vocab.schemas:
-                    for _ in range(entry.count):
-                        words.append(entry.word)
-                for entry in vocab.properties:
-                    for _ in range(entry.count):
-                        words.append(entry.word)
-                for entry in vocab.operations:
-                    for _ in range(entry.count):
-                        words.append(entry.word)
-                for entry in vocab.parameters:
-                    for _ in range(entry.count):
-                        words.append(entry.word)
-            return words
- 
-          # Put words in a map of frequencies.  
-        else:
-            for vocab in vocabs:
-              for entry in vocab.schemas:
-                  word = entry.word
-                  count= entry.count
-                  if word not in words:
-                      words[word] = 0
-                  words[word] += count
-              for entry in vocab.properties:
-                  word = entry.word
-                  count= entry.count
-                  if word not in words:
-                      words[word] = 0
-                  words[word] += count
-              for entry in vocab.operations:
-                  word = entry.word
-                  count= entry.count
-                  if word not in words:
-                      words[word] = 0
-                  words[word] += count
-              for entry in vocab.parameters:
-                  word = entry.word
-                  count= entry.count
-                  if word not in words:
-                      words[word] = 0
-                  words[word] += count
-
-                  
-            return words
+        words = []
+        for vocab in vocabs:   
+            for entry in vocab.schemas:
+                for _ in range(entry.count):
+                    words.append(entry.word)
+            for entry in vocab.properties:
+                for _ in range(entry.count):
+                    words.append(entry.word)
+            for entry in vocab.operations:
+                for _ in range(entry.count):
+                    words.append(entry.word)
+            for entry in vocab.parameters:
+                for _ in range(entry.count):
+                    words.append(entry.word)
+        return words
  
 
--- consistency/tool/clustering/word_extraction_test.py
+++ consistency/tool/clustering/word_extraction_test.py
@@ -1,60 +1,39 @@
-print("\033c")
-from glob import glob
 import unittest
 import json
 from mock import patch
 from word_extraction import ExtractWords
 from metrics import vocabulary_pb2
-from google.protobuf.json_format import Parse, ParseDict
-
-
+from google.protobuf.json_format import ParseDict
+from parameterized import parameterized
+ 
 class TestExtractWords(unittest.TestCase):
+ 
+    @parameterized.expand([
+        ('test1', ["ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd"]*2), 
+        ('test2', ["ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd"]*3 )
+    ])
     
-    global tests
-    def parse_json_to_vocab(self):
-
-        
-        with open('consistency/tool/clustering/word_extraction_test.json', 'r') as myfile:
-            data=myfile.read()
-
-        # parse file
-        ids = []
-        tests = []
-        obj = json.loads(data)
-        for key, value in obj.items():
-            ids.append(key)
-            test = []
-            for i in range (len(obj[key])):
-                vocab = vocabulary_pb2.Vocabulary()
-                fake_vocab = ParseDict(obj[key][i], vocab)
-                test.append(fake_vocab)
-            tests.append(test)
-        return ids, tests
-
     @patch.object(ExtractWords, 'extract_vocabs')
-    def test_fake_vocabs(self, mock_extract_vocabs):
-
-
-        longMessage = True 
-        ids, tests= self.parse_json_to_vocab()
-        for i in range(len(tests)):
-
-            mock_extract_vocabs.return_value = tests[i]
-
-            extrct = ExtractWords(stub = "stub", linearize=True)
-            actual = extrct.get_vocabs()
-
-            actual.sort()
-            expected = ["ab", "abc", "bc", "cd", "ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd", "ab", "ab", "bc", "cd"]*2
-            expected.sort()
-
-            self.assertEqual(actual, expected, "failed test: " + str(ids[i]) )
-
-
-
-
-
-
-
+    def test_vocab(self, name, expected, mock_extract_vocabs):
+        # PATCH
+        # Construct mock_response
+        mock_response = []
+        with open('word_extraction_test.json', 'r') as myfile:
+                data=myfile.read()
+        obj = json.loads(data)
+        for json_vocab in obj[name]:
+            vocab = vocabulary_pb2.Vocabulary()
+            mock_response.append(ParseDict(json_vocab, vocab))
+        mock_extract_vocabs.return_value = mock_response
+ 
+        #CALL
+        extrct = ExtractWords(stub = "stub")
+        actual = extrct.get_vocabs()
+        actual.sort()
+ 
+        # ASSERT
+        expected.sort()
+        self.assertEqual(actual, expected)
+ 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()

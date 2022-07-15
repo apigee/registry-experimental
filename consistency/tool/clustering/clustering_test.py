292e20158a57d33830e2128996a55b1cd94a111d
abenezer-gelaw <gelaw@google.com>
2022-07-15 16:56:46 -0700

    fixed

--- consistency/tool/clustering/clustering_test.py
+++ consistency/tool/clustering/clustering_test.py
@@ -6,12 +6,15 @@ from clustering import ClusterWords
 
 class TestClusterWords(unittest.TestCase):
 
+    with open('/home/gelaw/work-stuff/gocode/src/registry-experimental/consistency/tool/clustering/clustering_test.json', 'r') as f:
+        data = json.load(f)
+
     @parameterized.expand([
-        ('simple', ['abandon', 'abandoning', 'Abort', 'abort', 'aborted', 'About', 'about', 'Above',
-                                        'Absence', 'Absent', 'absentee', 'Absolute', 'absolutely', 'Abstain', 'Abuse',
-                                        'abusive', 'Accelerator', 'accelerator', 'accelerators', 'Accelerators',
-                                        'accept', 'acceptable', 'Accepted', 'accepts', 'Access', 'access', 'Accessed',
-                                        'Accesses', 'Accessibility'])
+        ('simple', {'abandon': ['abandon', 'abandoning'], 'Abort': ['Abort', 'abort', 'aborted'], 
+        'About': ['About', 'about'], 'Above': ['Above', 'Abstain', 'Abuse', 'Accepted', 'Accessibility', 'absentee', 'abusive'],
+        'Absence': ['Absence', 'Absent'], 'Absolute': ['Absolute', 'absolutely'],
+        'Accelerator': ['Accelerator', 'Accelerators', 'accelerator', 'accelerators'],
+        'accept': ['accept', 'acceptable', 'accepts'], 'Access': ['Access', 'Accessed', 'Accesses', 'access']})
     ])
 
     @patch.object(ClusterWords, 'get_words')
@@ -20,35 +23,19 @@ class TestClusterWords(unittest.TestCase):
         # PATCH
         # Construct mock_response
         
-        mock_get_words.return_value = ['abandon',  'abandoning', 'Abort', 'abort', 'aborted', 'About', 'about', 'Above',
-                                        'Absence', 'Absent', 'absentee', 'Absolute', 'absolutely', 'Abstain', 'Abuse',
-                                        'abusive', 'Accelerator', 'accelerator', 'accelerators', 'Accelerators',
-                                        'accept', 'acceptable', 'Accepted', 'accepts', 'Access', 'access', 'Accessed',
-                                        'Accesses', 'Accessibility', 'a' ]
+        mock_get_words.return_value = ["abandon", "abandoning", "Abort", "abort", "aborted", "About", "about", "Above",
+            "Absence","Absent", "absentee", "Absolute", "absolutely", "Abstain", "Abuse",
+            "abusive", "Accelerator", "accelerator", "accelerators", "Accelerators",
+            "accept", "acceptable", "Accepted", "accepts", "Access", "access", "Accessed",
+            "Accesses", "Accessibility"]
  
         #CALL
         clustr = ClusterWords(stub = "stub")
-        actual = clustr.clean_words()
+        actual = clustr.create_word_groups()
         print(actual)
         # ASSERT
-        self.assertListEqual(actual, expected)
-    # @parameterized.expand([
-    #     ('simple', ["ab",  "ab"])
-    # ])
-
-    # @patch.object(ClusterWords, 'get_words')
-    # def test_clean_Words(self, name, expected, mock_get_words):
-
-    #     # PATCH
-    #     # Construct mock_response
         
-    #     mock_get_words.return_value = ["ab", "ab", "a"]
- 
-    #     #CALL
-    #     clustr = ClusterWords(stub = "stub")
-    #     actual = clustr.clean_words()
-    #     # ASSERT
-    #     self.assertListEqual(actual, expected)
+        self.assertDictEqual(dict(sorted(actual.items())), dict(sorted(expected.items())))
  
  
 if __name__ == '__main__':

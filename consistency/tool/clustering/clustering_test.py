0fdc7de50af38670157fe44c241da617c4794f86
abenezer-gelaw <gelaw@google.com>
2022-07-15 17:12:45 -0700

    json file added

--- consistency/tool/clustering/clustering_test.py
+++ consistency/tool/clustering/clustering_test.py
@@ -6,11 +6,20 @@ from clustering import ClusterWords
 
 class TestClusterWords(unittest.TestCase):
 
-    with open('/home/gelaw/work-stuff/gocode/src/registry-experimental/consistency/tool/clustering/clustering_test.json', 'r') as f:
-        data = json.load(f)
+    with open('/home/gelaw/work-stuff/gocode/src/registry-experimental/consistency/tool/clustering/clustering_test.json', 'r') as myfile:
+            data=myfile.read()
+    obj = json.loads(data)
+    names = []
+    expected_values = []
+
+    for name in obj:
+        names.append(name)
+        expected_values.append(obj[name][0]["clusters"])
+
+
 
     @parameterized.expand([
-        ('simple', {'abandon': ['abandon', 'abandoning'], 'Abort': ['Abort', 'abort', 'aborted'], 
+        (names[0],{'abandon': ['abandon', 'abandoning'], 'Abort': ['Abort', 'abort', 'aborted'], 
         'About': ['About', 'about'], 'Above': ['Above', 'Abstain', 'Abuse', 'Accepted', 'Accessibility', 'absentee', 'abusive'],
         'Absence': ['Absence', 'Absent'], 'Absolute': ['Absolute', 'absolutely'],
         'Accelerator': ['Accelerator', 'Accelerators', 'accelerator', 'accelerators'],

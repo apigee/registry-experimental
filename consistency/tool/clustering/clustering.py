bf8ded921bc1e27f2979d609f645638283c616bc
abenezer-gelaw <gelaw@google.com>
2022-07-18 16:31:44 -0700

    cleaning tested

--- consistency/tool/clustering/clustering.py
+++ consistency/tool/clustering/clustering.py
@@ -9,101 +9,37 @@ from word_extraction import ExtractWords
 from collections import Counter
 import consistency.rpc.google.cloud.apigeeregistry.applications.v1alpha1.consistency.word_group_pb2 as wg
 
-from google.protobuf.json_format import ParseDict
 class ClusterWords:
-    def __init__(self, stub):
-      self.stub = stub
-
-    def get_words(self):
-        extrct = ExtractWords(stub="stub")
-        words = extrct.get_vocabs()
-
-        if words is None:
-            return None
-        return words
+    def __init__(self, stub, words):
+        self.stub = stub
+        self.words = words
 
     def clean_words(self):
-        words = self.get_words()
+        words = self.words
+        valid_words = []
 
         if words is None:
             return None
         for word in words:
-            if type(word) != str or len(word) < 2 or word != word:
-                words.remove(word)
-        words_length = len(words)
 
-        if words_length < 3:
+            # Each word needs to be at least of length 2 to form Dice bigrams. 
+            if type(word) == str and "." not in word and len(word) > 2:
+                valid_words.append(word)
+                print(word, "is valid")
+
+        words_length = len(valid_words)
+
+        # We need a minimum of 3 words to form a cluster.
+        if words_length < 3 and words_length > 0:
             print("Only ", words_length, " words found. Forming clusters not possible.")
-            return None
+            return valid_words
+        if words_length == 0:
+            print("No valid words detected.")
 
-        return words
+        self.words = valid_words
+        return valid_words
 
     def cluster(self):
-
-        words = self.clean_words()
-
-        if words is None:
-            return words
-
-        def extract_indices_dice(x, y):
-
-            dice = SorensenDice(2)
-            nonlocal words
-            i, j = int(x[0]), int(y[0])  
-            return dice.distance(words[i], words[j])
-
-        data = np.arange(len(words)).reshape(-1, 1)
-
-        try:
-            db = dbscan(data, metric=extract_indices_dice, eps=0.3, min_samples=2, algorithm='brute')
-        except Exception as e:
-            print(e, " Word Clustering Failed!")
-            return None
-
-        return words, db[1]
-    def create_word_groups(self):
-
-        words, labels = self.cluster()
-
-        _word_groups  = {}
-        for j in range(len(words)):
-            word_label = labels[j]
-
-            if word_label in _word_groups:
-                _word_groups[word_label].append(words[j])
-                        
-            else:
-                _word_groups[word_label] = [words[j]]
-
-        word_groups = {}
-
-        for k in _word_groups.keys():
-            similar_words = _word_groups[k]
-            if len(Counter(similar_words).most_common())  > 1:
-                word, count =Counter(similar_words).most_common()[0] 
-                if count > 1:
-                    word_groups[word] = similar_words
-                else:
-                    similar_words.sort()
-                    word_groups[similar_words[0]] = similar_words
-
-        return word_groups
-
-    # def vocabulary_upload(self):
-    #     _word_groups = self.create_word_groups()
-    #     wordGroup = wg.WordGroup()
-    #     for key, val in _word_groups.items():
-    #         word_group = {}
-    #         word_group["id"] = key
-    #         word_group["kind"] = "kind"
-    #         word_group["word_frequency"] = dict(Counter(val))
-    #         ParseDict(word_group,  wordGroup)
-
-    #         #to do 
-    #         # upload the parsed wordGroup to registry 
-    #         # test by patching ExtractVocabs
-    #         # 
-
 
         
 
--- consistency/tool/clustering/clustering_test.py
+++ consistency/tool/clustering/clustering_test.py
@@ -1,26 +1,14 @@
+from imghdr import tests
 import unittest
 import json
-from mock import patch
 from parameterized import parameterized
 from clustering import ClusterWords
 import os
 class TestClusterWords(unittest.TestCase):
-    ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 
-    with open(os.path.join(ROOT_DIR, 'clustering', 'clustering_test.json'), 'r') as myfile:
-            data=myfile.read()
-    obj = json.loads(data)
-    names = []
-    expected_values = []
-
-    for name in obj:
-        names.append(name)
-        expected_values.append(obj[name]["clusters"])
-
-    @parameterized.expand(zip(names, expected_values))
-
-    @patch.object(ClusterWords, 'get_words')
-    def test_clustering(self, name, expected, mock_get_words):
+    @parameterized.expand(["simple-test", "int-test", "short-test", "None-test", "dot-test"])
+    
+    def test_clustering(self, name):
 
         # PATCH
         # Construct mock_response
@@ -28,15 +16,14 @@ class TestClusterWords(unittest.TestCase):
         with open(os.path.join(ROOT_DIR, 'clustering', 'clustering_test.json'), 'r') as myfile:
                 data=myfile.read()
         obj = json.loads(data)
-    
-        mock_get_words.return_value = obj[name]["words"]
-
+        mock_words  = obj['clean-words'][name]["words"]
+        expected = obj['clean-words'][name]["cleaned-words"]
         #CALL
-        clustr = ClusterWords(stub = "stub")
-        actual = clustr.create_word_groups()
+        clustr = ClusterWords(stub = "stub", words=mock_words)
+        actual = clustr.clean_words()
 
         # ASSERT
-        self.assertDictEqual(dict(sorted(actual.items())), dict(sorted(expected.items())))
+        self.assertEqual(actual,  expected)
  
  
 if __name__ == '__main__':

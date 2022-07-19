fc0849810fe33507918e149bf1d8c7eda22b5dbd
abenezer-gelaw <gelaw@google.com>
2022-07-18 23:48:38 -0700

    final clean up

---
+++ consistency/tool/clustering/clustering_test.py
@@ -0,0 +1,29 @@
+import unittest
+import json
+from parameterized import parameterized
+from clustering import ClusterWords
+import os
+class TestClusterWords(unittest.TestCase):
+
+    @parameterized.expand(["simple-test", "int-test", "short-test", "None-test", "dot-test"])
+
+    def test_clustering(self, name):
+
+        # PATCH
+        # Construct mock_response
+        ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+        with open('clustering_test.json', 'r') as myfile:
+                data=myfile.read()
+        obj = json.loads(data)
+        mock_words  = obj['clean-words'][name]["words"]
+        expected = obj['clean-words'][name]["cleaned-words"]
+        #CALL
+        clustr = ClusterWords(stub = "stub", words=mock_words)
+        clustr.clean_words()
+
+        # ASSERT
+        self.assertEqual(clustr.words,  expected)
+
+
+if __name__ == '__main__':
+    unittest.main() 

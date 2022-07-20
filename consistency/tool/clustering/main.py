6ded5755f2a45ca3eeacd360fa7468eae4304339
abenezer-gelaw <gelaw@google.com>
2022-07-20 16:34:19 -0700

    basic connection

--- consistency/tool/clustering/main.py
+++ consistency/tool/clustering/main.py
@@ -7,17 +7,18 @@ def main ():
    # Creating registry client
    channel = grpc.insecure_channel('localhost:8080')
    stub = registry_service_pb2_grpc.RegistryStub(channel)
-   extrct = ExtractWords( stub=stub)
+   extrct = ExtractWords(stub=stub)
    try:
        words = extrct.get_vocabs()
    except Exception as e:
-               print(e, " Getting words failed")
+               print(e, " \n Getting words failed")
  
    clustr = ClusterWords(stub = stub, words=words)
    try:
        word_grups = clustr.create_word_groups()
+       print(word_grups)
    except Exception as e:
-               print(e, " Clustering words failed")
+               print(e, " \n Clustering words failed")
  
  
 if __name__ == '__main__':

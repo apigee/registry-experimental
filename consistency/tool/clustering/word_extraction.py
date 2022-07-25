815f9d9303e91535169b0c303f35fd9bbce675a2
abenezer-gelaw <gelaw@google.com>
2022-07-24 17:07:39 -0700

    irreproducible bug on getting ints instead of strs

--- consistency/tool/clustering/word_extraction.py
+++ consistency/tool/clustering/word_extraction.py
@@ -4,35 +4,34 @@ from metrics import vocabulary_pb2
 
 
 class ExtractWords:
-    def __init__(self, stub):
+    def __init__(self, stub, project_name):
         self.stub = stub
+        self.project_name = project_name
 
     def extract_vocabs(self):
         stub = self.stub
         try:
             response = stub.ListArtifacts(
                 registry_service_pb2.ListArtifactsRequest(
-                    parent="projects/-/locations/global/apis/-/versions/-/specs/-",
-                    filter='name.contains("vocabulary")',
-                )
-            )
+                    parent="projects/" + self.project_name + "/locations/global/apis/-/versions/-/specs/-",
+                    filter="name.contains(\"vocabulary\")"
+                ))
         except grpc.RpcError as rpc_error:
-            print(
-                f"Failed to fetch vocabulary artifacts, RPC error: code={rpc_error.code()} message={rpc_error.details()}"
-            )
-            return None
+            print(f"Received RPC error: code={rpc_error.code()} message={rpc_error.details()}")
 
         vocabs = []
         for artifact in response.artifacts:
             contents = stub.GetArtifactContents(
-                registry_service_pb2.GetArtifactContentsRequest(name=artifact.name)
+                registry_service_pb2.GetArtifactContentsRequest(
+                    name=artifact.name,
+                )
             )
             vocab = vocabulary_pb2.Vocabulary()
-
+            
             try:
                 vocabs.append(vocab.ParseFromString(contents.data))
             except Exception as e:
-                print(e, " Parsing contents for ", artifact.name, "failed")
+                print(e, " Getting vocabulary for ", artifact.name, "failed!")
                 continue
 
         if len(vocabs) < 1:
@@ -41,7 +40,7 @@ class ExtractWords:
         return vocabs
 
     def get_vocabs(self):
-        vocabs = self.extract_vocabs(self)
+        vocabs = self.extract_vocabs()
         if vocabs is None:
             return None
 

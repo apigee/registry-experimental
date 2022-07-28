eb722fd11c49af1658fd1d4447dd74219c4b6b80
abenezer-gelaw <gelaw@google.com>
2022-07-27 23:34:01 -0700

    comparison basic testing passes

--- consistency/tool/comparison/comparison_test.py
+++ consistency/tool/comparison/comparison_test.py
@@ -12,78 +12,78 @@ from google.cloud.apigeeregistry.applications.v1alpha1.consistency import (
 from google.protobuf.json_format import ParseDict
 from strsimpy import SorensenDice
 class TestComparison(unittest.TestCase):
-    # @parameterized.expand(
-    #     [
-    #         "simple",
-    #         "none-wordgroups",
-    #         "none-words",
-    #         "both-null",
-    #     ]  # , "unique-terms", "both-null"]
-    # )
-    # def test_find_word_groups(
-    #     self,
-    #     name,
-    # ):
+    @parameterized.expand(
+        [
+            "simple",
+            "none-wordgroups",
+            "none-words",
+            "both-null",
+        ]  # , "unique-terms", "both-null"]
+    )
+    def test_find_word_groups(
+        self,
+        name,
+    ):
 
-    #     # PATCH
-    #     # Construct mock_response
-    #     with open("comparison_test.json", "r") as myfile:
-    #         data = myfile.read()
-    #     obj = json.loads(data)
-    #     test_suite = obj[name]
-    #     input_wordgroups = []
-    #     if test_suite["wordgroups"] == None or test_suite["words"] == None:
-    #         return None
-    #     for i in test_suite["wordgroups"]:
-    #         wrd_grp = wg.WordGroup()
-    #         if i != None:
-    #             input_wordgroups.append(ParseDict(i, wrd_grp))
-    #         else:
-    #             input_wordgroups.append(None)
+        # PATCH
+        # Construct mock_response
+        with open("comparison_test.json", "r") as myfile:
+            data = myfile.read()
+        obj = json.loads(data)
+        test_suite = obj[name]
+        input_wordgroups = []
+        if test_suite["wordgroups"] == None or test_suite["words"] == None:
+            return None
+        for i in test_suite["wordgroups"]:
+            wrd_grp = wg.WordGroup()
+            if i != None:
+                input_wordgroups.append(ParseDict(i, wrd_grp))
+            else:
+                input_wordgroups.append(None)
 
-    #     # CALL
-    #     cmprsn = Comparison(
-    #         stub="stub", new_words=test_suite["words"], word_groups=input_wordgroups
-    #     )
-    #     actual = cmprsn.find_closest_word_groups()
-    #     expected = {}
-    #     for word, comparison_info in test_suite["expected"].items():
-    #         wrd_grp = wg.WordGroup()
-    #         ParseDict(comparison_info[0], wrd_grp)
-    #         expected[word] = [wrd_grp, comparison_info[1]]
+        # CALL
+        cmprsn = Comparison(
+            stub="stub", new_words=test_suite["words"], word_groups=input_wordgroups
+        )
+        actual = cmprsn.find_closest_word_groups()
+        expected = {}
+        for word, comparison_info in test_suite["expected"].items():
+            wrd_grp = wg.WordGroup()
+            ParseDict(comparison_info[0], wrd_grp)
+            expected[word] = [wrd_grp, comparison_info[1]]
 
-    #     # ASSERT
-    #     self.assertDictEqual(actual, expected)
+        # ASSERT
+        self.assertDictEqual(actual, expected)
 
-    # @parameterized.expand(["unique-terms"])  # , "unique-terms", "both-null"]
-    # def test_find_word_groups_unique(
-    #     self,
-    #     name,
-    # ):
+    @parameterized.expand(["unique-terms"])  # , "unique-terms", "both-null"]
+    def test_find_word_groups_unique(
+        self,
+        name,
+    ):
 
-    #     # PATCH
-    #     # Construct mock_response
-    #     with open("comparison_test.json", "r") as myfile:
-    #         data = myfile.read()
-    #     obj = json.loads(data)
-    #     test_suite = obj[name]
-    #     input_wordgroups = []
-    #     if test_suite["wordgroups"] == None or test_suite["words"] == None:
-    #         return None
-    #     for i in test_suite["wordgroups"]:
-    #         wrd_grp = wg.WordGroup()
-    #         if i != None:
-    #             input_wordgroups.append(ParseDict(i, wrd_grp))
-    #         else:
-    #             input_wordgroups.append(None)
+        # PATCH
+        # Construct mock_response
+        with open("comparison_test.json", "r") as myfile:
+            data = myfile.read()
+        obj = json.loads(data)
+        test_suite = obj[name]
+        input_wordgroups = []
+        if test_suite["wordgroups"] == None or test_suite["words"] == None:
+            return None
+        for i in test_suite["wordgroups"]:
+            wrd_grp = wg.WordGroup()
+            if i != None:
+                input_wordgroups.append(ParseDict(i, wrd_grp))
+            else:
+                input_wordgroups.append(None)
 
-    #     # CALL
-    #     cmprsn = Comparison(
-    #         stub="stub", new_words=test_suite["words"], word_groups=input_wordgroups
-    #     )
-    #     actual = cmprsn.find_closest_word_groups()
-    #     expected = test_suite["expected"]
-    #     self.assertDictEqual(actual, expected)
+        # CALL
+        cmprsn = Comparison(
+            stub="stub", new_words=test_suite["words"], word_groups=input_wordgroups
+        )
+        actual = cmprsn.find_closest_word_groups()
+        expected = test_suite["expected"]
+        self.assertDictEqual(actual, expected)
 
 
     # Compparison Report test
